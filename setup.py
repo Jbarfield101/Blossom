285f615284521a536b2a01637883d56f9d57ec43
Jbarfield101 <Jbarfield101@gmail.com>
2025-08-15 17:32:49 -0400

    feat: expose app version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='blossom-audio',
-    version='0.1.0',
+    version='0.1.5',
     install_requires=[
         'scipy>=1.9.0',
         'pyloudnorm>=0.1.0',
---
+++ src/version.ts
@@ -0,0 +1,6 @@
+// Expose the application version from package.json
+import { version } from "../package.json";
+
+export const appVersion = version;
+export default appVersion;
+
