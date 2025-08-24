d41463a034eb85c45c18ebfe69b94857f827ca13
Jbarfield101 <Jbarfield101@gmail.com>
2025-08-24 01:54:37 -0400

    Use refresh_cpu_usage for CPU metrics

--- src-tauri/src/task_queue.rs
+++ src-tauri/src/task_queue.rs
@@ -145,9 +145,9 @@ impl TaskQueue {
                                     (l.cpu, l.memory)
                                 };
                                 let mut sys = System::new();
-                                sys.refresh_cpu_all();
+                                sys.refresh_cpu_usage();
                                 std::thread::sleep(Duration::from_millis(100));
-                                sys.refresh_cpu_all();
+                                sys.refresh_cpu_usage();
                                 sys.refresh_memory();
                                 let cpu_usage = sys.global_cpu_usage();
                                 let mem_usage = if sys.total_memory() > 0 {
