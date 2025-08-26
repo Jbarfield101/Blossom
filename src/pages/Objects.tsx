77fbea58916406d535cefbbfe11f8a9eec06f1b2
Jbarfield101 <Jbarfield101@gmail.com>
2025-08-26 16:41:45 -0400

    Replace cube with blender icon on objects page

--- src/pages/Objects.tsx
+++ src/pages/Objects.tsx
@@ -1,24 +1,20 @@
-import { Canvas } from "@react-three/fiber";
+import { FaBlender } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
-function Cube({ onSelect }:{ onSelect:()=>void }){
-  return (
-    <mesh onClick={onSelect} style={{ cursor:"pointer" }}>
-      <boxGeometry args={[1,1,1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  );
-}
-
-export default function Objects(){
+export default function Objects() {
   const nav = useNavigate();
   return (
-    <div style={{ height:"100vh" }}>
-      <Canvas camera={{ position:[3,3,3] }}>
-        <ambientLight />
-        <pointLight position={[10,10,10]} />
-        <Cube onSelect={()=>nav("/objects/blender")} />
-      </Canvas>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+      onClick={() => nav("/objects/blender")}
+    >
+      <FaBlender size={64} />
     </div>
   );
 }
