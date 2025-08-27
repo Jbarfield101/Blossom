ee864f7c20d3b7054b3d8e15ca4422d25a79c57c
Jbarfield101 <Jbarfield101@gmail.com>
2025-08-27 18:39:25 -0400

    Add splash screen with flower animation

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,24 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import { ThemeProvider } from "./features/theme/ThemeContext";
+import SplashScreen from "./components/SplashScreen";
+
+function Root() {
+  const [ready, setReady] = React.useState(false);
+
+  React.useEffect(() => {
+    const id = requestAnimationFrame(() => setReady(true));
+    return () => cancelAnimationFrame(id);
+  }, []);
+
+  return ready ? <App /> : <SplashScreen />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
       <ThemeProvider>
-        <App />
+        <Root />
       </ThemeProvider>
     </HashRouter>
   </React.StrictMode>
