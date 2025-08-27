f75ff7fb153bc925db11e58c0b3f704336ed784b
Jbarfield101 <Jbarfield101@gmail.com>
2025-08-27 18:39:47 -0400

    Maximize Tauri window at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { appWindow } from "@tauri-apps/api/window";
 import App from "./App";
 import "./styles.css";
 import { ThemeProvider } from "./features/theme/ThemeContext";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+function Root() {
+  useEffect(() => {
+    appWindow.maximize();
+  }, []);
+
+  return (
     <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </HashRouter>
-  </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
 );
+
